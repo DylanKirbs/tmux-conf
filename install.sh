683e9926bc71dfe28dd2bf948d4c662bcfb1f999
Dylan Kirby <dylan.kirby.365@gmail.com>
2023-08-11 09:41:13 +0200

    Updates to installer script

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ while [[ $# -gt 0 ]]; do
             echo "Usage: ./install.sh [options]"
             echo "Options:"
             echo "  -h, --help      Display this help message"
+            ;;
         *)
             echo "Unknown option: $key"
             exit 1
@@ -20,17 +21,28 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+# Making sure system requirements are met
+sudo apt update
+sudo apt install tmux
+sudo apt install curl
+
 echo "Installing .tmux.conf file..."
 cp .tmux.conf ~/
 
 echo "Installing tpm..."
+mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 
-echo "Installing poweline theme..."
-cp -r powerline/themes/ ~/.tmux/plugins/tmux-powerline/
-
 # source the .tmux.conf file
-echo "Run the command in tmux to source .tmux.conf file..."
-echo tmux source-file ~/.tmux.conf
+echo "Configuring tmux..."
+tmux new -d -s configSession
+tmux send-keys -t configSession.0 "tmux source-file ~/.tmux.conf" ENTER
+
+echo "Installing tmux plugins..."
+tmux send-keys -t configSession.0 C-I ENTER
+tmux send-keys -t configSession.0 "exit" ENTER
+
+echo "Configuring Powerline theme"
+cp -r powerline/themes/* ~/.tmux/plugins/tmux-powerline/themes
 
 echo "Done!"
